db6db0dcecee618f0eb386199874fd9400977519
yash-251198 <shetyeyash444@gmail.com>
2025-04-05 21:37:22 +0530

    initial commit  with setup of client side and  creation of login page ui

---
+++ vibechatt/src/App.jsx
@@ -0,0 +1,23 @@
+
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Auth from './pages/auth/Auth'
+import Chat from './pages/chat/Chat'
+import Profile from './pages/profile/Profile'
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/auth' element={<Auth/>}/>
+          <Route path='/chat' element={<Chat/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='*' element={<Navigate to='/auth'/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ vibechatt/src/pages/auth/Auth.jsx
@@ -0,0 +1,71 @@
+import Background from '@/assets/login2.png'
+import Victory from '@/assets/victory.svg'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { TabsContent } from '@radix-ui/react-tabs'
+import { useState } from 'react'
+
+
+const Auth = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+
+    const handelLogin = async () => {
+
+    }
+
+    const handleSignUp = async () => {
+
+    }
+
+    return (
+        <div className="h-[100vh] w-[100vw] flex items-center justify-center">
+            <div className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2">
+                <div className="flex flex-col gap-10 items-center justify-center">
+                    <div className="flex items-center justify-center flex-col">
+                        <div className="flex items-center justify-center">
+                            <h1 className="text-5xl font-bold md:text-6xl">
+                                Welcome
+                            </h1>
+                            <img src={Victory} alt="victory emoji" className='h-[100px]' />
+                        </div>
+                        <p className='font-medium text-center'>
+                            Fill in the details to get started with a best chat app
+                        </p>
+                    </div>
+                    <div className="flex items-center justify-center w-full">
+                        <Tabs className="w-3/4">
+                            <TabsList className="bg-transparent rounded-none w-full">
+                                <TabsTrigger className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300" value="login">Login</TabsTrigger>
+                                <TabsTrigger className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300" value="signup">SignUp</TabsTrigger>
+                            </TabsList>
+                            <TabsContent className='flex flex-col gap-5 mt-10' value='login'>
+                                <Input placeholder="Email" type="email" className="rounded-full p-6" value={email} onChange={(e) => setEmail(e.target.value)}>
+                                </Input>
+                                <Input placeholder="Password" type="password" className="rounded-full p-6" value={password} onChange={(e) => setPassword(e.target.value)}>
+                                </Input>
+                                <Button className="rounded-full p-6" onClick={handelLogin}>Login</Button>
+                            </TabsContent>
+                            <TabsContent className='flex flex-col gap-5 mt-10' value='signup'>
+                                <Input placeholder="Email" type="email" className="rounded-full p-6" value={email} onChange={(e) => setEmail(e.target.value)}>
+                                </Input>
+                                <Input placeholder="Password" type="password" className="rounded-full p-6" value={password} onChange={(e) => setPassword(e.target.value)}>
+                                </Input>
+                                <Input placeholder="Confirm Password" type="password" className="rounded-full p-6" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}>
+                                </Input>
+                                <Button className="rounded-full p-6" onClick={handleSignUp}>SignUp</Button>
+                            </TabsContent>
+                        </Tabs>
+                    </div>
+                </div>
+                <div className="hidden xl:flex justify-center items-center">
+                    <img src={Background} alt="Background Login" className='h-[500px]'/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Auth
