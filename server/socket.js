76b216ba2613ec0bff43286e7d1cd1bc9f4d27b0
yash-251198 <shetyeyash444@gmail.com>
2025-04-26 15:38:50 +0530

    added logic for handling signling related the voicecall between users

--- server/socket.js
+++ server/socket.js
@@ -1,9 +1,9 @@
-import { Socket, Server as SocketIoSever } from 'socket.io'
+import { Server as SocketIoServer } from 'socket.io';
 import Message from './model/MessageModel.js';
 import Channel from './model/channelModel.js';
 
 const setupSocket = (server) => {
-    const io = new SocketIoSever(server, {
+    const io = new SocketIoServer(server, {
         cors: {
             origin: process.env.ORIGIN,
             methods: ["GET", "POST"],
@@ -12,39 +12,55 @@ const setupSocket = (server) => {
     });
 
     const userSocketMap = new Map();
+    const activeCallMap = new Map();
 
     const disconnect = (socket) => {
-        console.log(`client disconnect ${socket.id}`);
-
-        for (const [userId, soketId] of userSocketMap.entries()) {
-            if (soketId === socket.id) {
+        // Find user who disconnected
+        let disconnectedUserId = null;
+        for (const [userId, socketId] of userSocketMap.entries()) {
+            if (socketId === socket.id) {
+                disconnectedUserId = userId;
                 userSocketMap.delete(userId);
+                break;
             }
         }
-    }
 
+        // If user was in a call, notify the other party
+        if (disconnectedUserId && activeCallMap.has(disconnectedUserId)) {
+            const callInfo = activeCallMap.get(disconnectedUserId);
+            const otherPartySocketId = userSocketMap.get(callInfo.peerId);
+
+            if (otherPartySocketId) {
+                io.to(otherPartySocketId).emit("callEnded", {
+                    message: "Call ended because user disconnected"
+                });
+            }
+
+            activeCallMap.delete(disconnectedUserId);
+            activeCallMap.delete(callInfo.peerId);
+        }
+    };
 
     const sendMessage = async (message) => {
-        const senderSocketId = userSocketMap.get(message.sender)
-        const recieverSocketId = userSocketMap.get(message.reciever)
+        const senderSocketId = userSocketMap.get(message.sender);
+        const recieverSocketId = userSocketMap.get(message.reciever);
 
         const createdMessage = await Message.create(message);
 
-        const messageData = await Message.findById(createdMessage._id).
-            populate("sender", "id email firstName lastName image theme")
+        const messageData = await Message.findById(createdMessage._id)
+            .populate("sender", "id email firstName lastName image theme")
             .populate("reciever", "id email firstName lastName image theme");
-
 
         [senderSocketId, recieverSocketId].forEach(socketId => {
             if (socketId) {
                 io.to(socketId).emit("recieveMessage", messageData);
             }
         });
-    }
+    };
 
     const sendChannelMsg = async (message) => {
         const { channelId, sender, content, messageType, fileUrl } = message;
-        
+
         const createMsg = await Message.create({
             sender,
             reciever: null,
@@ -52,55 +68,132 @@ const setupSocket = (server) => {
             messageType,
             timestamp: new Date(),
             fileUrl
-        })
-        
-        const messageData = await Message.findById(createMsg._id).populate("sender", "id email firstName lastName image theme").exec()
-        
+        });
+
+        const messageData = await Message.findById(createMsg._id)
+            .populate("sender", "id email firstName lastName image theme")
+            .exec();
+
         await Channel.findByIdAndUpdate(channelId, {
             $push: { messages: createMsg._id }
         });
-        
+
         const channel = await Channel.findById(channelId).populate("members");
-        
+
         const finaldata = { ...messageData._doc, channelId: channel._id };
-        
+
         if (channel && channel.members) {
             channel.members.forEach((member) => {
-                const memberSocketId = userSocketMap.get(member._id.toString())
-                
+                const memberSocketId = userSocketMap.get(member._id.toString());
+
                 if (memberSocketId) {
-                    io.to(memberSocketId).emit("recieve-channel-message", finaldata)
+                    io.to(memberSocketId).emit("recieve-channel-message", finaldata);
                 }
             });
-            
-            const adminSocketId = userSocketMap.get(channel.admin._id.toString())
+
+            const adminSocketId = userSocketMap.get(channel.admin._id.toString());
             if (adminSocketId) {
-                io.to(adminSocketId).emit("recieve-channel-message", finaldata)
+                io.to(adminSocketId).emit("recieve-channel-message", finaldata);
             }
         }
-    }
+    };
+
+    const outGoingCall = async (data) => {
+        const { from, to } = data;
+        const calleesocketId = userSocketMap.get(to._id);
+        const callersocketId = userSocketMap.get(from.id);
+        if (calleesocketId) {
+            io.to(calleesocketId).emit("incomingCall", data);
+        } else {
+            io.to(callersocketId).emit("callEnded", {
+                message: "Call ended because user disconnected"
+            });
+        }
+    };
+
+    const callAccepted = async (data) => {
+        const { to, from} = data;
+        const callerSocketId = userSocketMap.get(to);
+
+        if (callerSocketId) {
+            // Update call mapping for the receiver
+            if (activeCallMap.has(to)) {
+                activeCallMap.set(from, {
+                    callType: activeCallMap.get(to).callType,
+                    peerId: to
+                });
+            }
+
+            // Forward acceptance to caller
+            io.to(callerSocketId).emit("callAccepted", {
+                from
+            });
+        }
+    };
+
+    const endCall = async (data) => {
+        const { to, from } = data;
+
+        // Clean up call mappings
+        activeCallMap.delete(to);
+        activeCallMap.delete(from);
+
+        const receiverSocketId = userSocketMap.get(to);
+
+        if (receiverSocketId) {
+            io.to(receiverSocketId).emit("callEnded", {
+                from
+            });
+        }
+    };
+
+    const callRejected = async (data) => {
+        const { to, from} = data;
+        const callerSocketId = userSocketMap.get(to);
+
+        if (callerSocketId) {
+            // Clean up call mappings
+            activeCallMap.delete(to);
+            activeCallMap.delete(from);
+
+            // Notify caller of rejection
+            io.to(callerSocketId).emit("callRejected", {
+                from,
+                reason:"Call was rejected"
+            });
+        }
+    };
 
     io.on("connection", (socket) => {
         const userId = socket.handshake.query.userId;
 
         if (userId) {
             userSocketMap.set(userId, socket.id);
-            // console.log("User connected-" + userId + " socket id-" + socket.id)
+            console.log(`User connected - ${userId} (Socket ID: ${socket.id})`);
         } else {
-            console.log("userid not provided during connection");
+            console.log("User ID not provided during connection");
         }
 
-        socket.on("sendMessage", sendMessage)
-        socket.on("send-channel-msg", sendChannelMsg)
-        socket.on("disconnect", () => disconnect(socket))
+        socket.on("sendMessage", sendMessage);
+        socket.on("send-channel-msg", sendChannelMsg);
+        socket.on("disconnect", () => disconnect(socket));
 
-        /* socket.on("incoming",incomingCall);
-        socket.on("outgoing",outgoingCall);
-        socket.on("accept",accept);
-        socket.on("reject",reject); */
+        socket.on("outgoingCall", outGoingCall);
+        socket.on("call-accepted", callAccepted);
+        socket.on("call-rejected", callRejected);
+        socket.on("endCall", endCall);
 
-    })
-
-}
+        // Handle ICE candidates
+        socket.on('iceCandidate', ({ to, from, candidate }) => {
+            const receiverSocketId = userSocketMap.get(to);
+            if (receiverSocketId) {
+                io.to(receiverSocketId).emit('iceCandidate', {
+                    from,
+                    candidate
+                });
+            }
+        });
+    });
+};
 
 export default setupSocket;
