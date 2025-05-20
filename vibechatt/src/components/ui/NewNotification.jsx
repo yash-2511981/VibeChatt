b9ec82c5a482fd9035856c2a44fd9b2017ec3245
yash-251198 <shetyeyash444@gmail.com>
2025-05-21 02:08:56 +0530

    working on new-notification ui

--- vibechatt/src/components/ui/NewNotification.jsx
+++ vibechatt/src/components/ui/NewNotification.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { IoMdSend } from 'react-icons/io'
+import { IoChatbubble, IoChatbubblesOutline, IoMic, IoNotifications, IoNotificationsCircleOutline, IoNotificationsOffCircleOutline, IoNotificationsOutline, IoSendOutline, IoSendSharp, IoText } from 'react-icons/io5'
+import { MdDoorbell, MdSend, MdSendToMobile } from 'react-icons/md'
+import { RiBellFill, RiNotification2Fill, RiNotification2Line } from 'react-icons/ri'
 import { toast } from 'sonner'
 const contact = {
     email:
@@ -26,24 +30,34 @@ const contact = {
 export const newMessageArrive = () => {
 
 
-    toast.custom(() => {
+    toast.custom((t) => {
         return (
-            <div className="bg-gray-700 text-white rounded-lg shadow-xl border border-gray-600 overflow-hidden w-full max-w-sm p-1">
-                <span className='text-sm'>New Message</span>
-                <div className='p-1 gap-2 flex items-center justify-center'>
+            <div className="bg-gray-700 text-white rounded-lg shadow-xl border border-gray-600 overflow-hidden w-55 flex items-center p-1">
+                <div className='relative'>
+                    <IoNotifications className='text-3xl' />
+                    <span className='absolute top-[-3px] right-0 bg-green-500 rounded-full h-4 w-4 flex items-center justify-center'>{contact.unseenCount}</span>
+                </div>
+                <div className=' ml-2 gap-2 flex items-center justify-center'>
                     <div className='text-Start'>
                         <div className='text-sm'>
                             {contact?.firstName && contact?.lastName ?
                                 `${contact.firstName} ${contact.lastName}` : `${contact.email}`}
-                            <span>({contact.unseenCount})</span>
                         </div>
-                        <div className='text-xs'>
+                        <div className='text-xs flex items-center'>
+                            <IoMic className='mr-1 ml-0' />
                             {contact.lastMessage.fileUrl.split("/").pop().slice(0, 20)}
                         </div>
                     </div>
                 </div>
+                <div className='ml-4 cursor-pointer text-[10px] text-purple-790'>
+                    Reply
+                </div>
             </div>
         );
+    }, {
+        duration: 3000,
+        position: 'top-right',
+        closeButton: true
     })
 }
 
